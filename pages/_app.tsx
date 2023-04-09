d921044281fdeae3515b72d5821bf20a32be3dda
Apurva Shah <shahh.apurva@gmail.com>
2023-04-10 00:33:34 +0100

    Fixed Bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,6 @@ import "@/styles/main.css";
 
 import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
