cb553dabdad3ed7116be199fe573f520bacf45c2
Apurva Shah <shahh.apurva@gmail.com>
2023-04-11 01:22:56 +0100

    Added way more projects 1/2

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -14,7 +14,7 @@ function Projects({ overwriteProjects }: ProjectProps) {
   const projectsList = overwriteProjects ? overwriteProjects : projects;
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3 items-start">
-      {projectsList.map((item) => {
+      {projectsList.slice(0).reverse().map((item) => {
         return <ProjectCard key={item.id} project={item} />;
       })}
     </div>
