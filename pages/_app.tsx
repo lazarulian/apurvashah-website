a7a25dd9971f7d22c915e819f1a662aa297751ac
Apurva Shah <shahh.apurva@gmail.com>
2023-12-25 00:15:07 -0500

    feat(about): added a little flavor to the about section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import "tailwindcss/tailwind.css";
 import "@/styles/main.css";
+import "@/styles/map.css";
 
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,19 +1,15 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Blog = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const destination = 'https://blog.apurvashah.org'; // Replace this with the desired external URL
+    const destination = "https://blog.apurvashah.org"; // Replace this with the desired external URL
     window.location.href = destination;
   }, []);
 
-  return (
-    <div>
-      Redirecting...
-    </div>
-  );
+  return <div>Redirecting...</div>;
 };
 
 export default Blog;
