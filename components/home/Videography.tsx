4485125b701a9f153ab2fe4a9f3b40fc51539d67
Apurva Shah <shahh.apurva@gmail.com>
2024-12-11 11:01:56 -0800

    hotfix

--- components/home/Videography.tsx
+++ components/home/Videography.tsx
@@ -21,13 +21,6 @@ const Videography = () => {
           ></iframe>
         </div>
       </div>
-      <div className="relative w-full mt-2">
-        <Link href="/projects">
-          <div className="mt-8 max-w-sm md:max-w-2xl border border-fun-pink mx-auto text-center w-full whitespace-nowrap px-8 py-3 rounded-full text-fun-pink bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white transition-colors cursor-pointer">
-            View All
-          </div>
-        </Link>
-      </div>
     </div>
   );
 };
